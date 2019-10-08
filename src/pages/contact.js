302cc409fba6ac7b0db33513776f26319d10d10c
Beau Jackson <beau.html@gmail.com>
2019-10-08 13:46:50 -0400

    adds temporary contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,11 @@
 import React from 'react';
 // import { Link } from 'gatsby';
-import ContactForm from '../components/subcomponents/ContactForm'
+import ContactForm from '../components/subcomponents/ContactForm2'
 
 import Layout from '../components/Layout'
 
 const Contact = () => (
     <Layout>
-        <h1>Contact Me</h1>
         <h2>This is the Contact page</h2>
         {/* <Link to="/">&larr; back to home</Link> */}
         <ContactForm/>
